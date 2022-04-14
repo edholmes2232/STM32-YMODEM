63cef4d99e3a8929ab5a8929703bf7d83810f199
Ed Holmes <eholmes@lablogic.com>
2022-04-14 17:19:06 +0100

    Clean up defines

--- ymodem_conf_template.h
+++ ymodem_conf_template.h
@@ -6,18 +6,18 @@
 
 /**
  * @brief  Select STM32 Device family used. Options are:
- * 		 DEVICE_FAMILY_STM32F0
- * 		 DEVICE_FAMILY_STM32F1
- * 		 DEVICE_FAMILY_STM32F2
- * 		 DEVICE_FAMILY_STM32F3
- * 		 DEVICE_FAMILY_STM32F4
- * 		 DEVICE_FAMILY_STM32F7
- * 		 DEVICE_FAMILY_STM32G0
- * 		 DEVICE_FAMILY_STM32G4
- * 		 DEVICE_FAMILY_STM32H7
- * 		 DEVICE_FAMILY_STM32L0
- * 		 DEVICE_FAMILY_STM32L1
- * 		 DEVICE_FAMILY_STM32L4
+ * 		   - DEVICE_FAMILY_STM32F0
+ * 		   - DEVICE_FAMILY_STM32F1
+ * 		   - DEVICE_FAMILY_STM32F2
+ * 		   - DEVICE_FAMILY_STM32F3
+ * 		   - DEVICE_FAMILY_STM32F4
+ * 		   - DEVICE_FAMILY_STM32F7
+ * 		   - DEVICE_FAMILY_STM32G0
+ * 		   - DEVICE_FAMILY_STM32G4
+ * 		   - DEVICE_FAMILY_STM32H7
+ * 		   - DEVICE_FAMILY_STM32L0
+ * 		   - DEVICE_FAMILY_STM32L1
+ * 		   - DEVICE_FAMILY_STM32L4
  * 
  */
 #define DEVICE_FAMILY_STM32F7
@@ -26,14 +26,14 @@
  * @brief	Whether to check programming was successful
  * 
  */
-//#define YMODEM_VALIDATE_PROGRAMMING		0
+//#define YMODEM_VALIDATE_PROGRAMMING	
 
 
 /**
  * @brief	Size of flash to save file to  
  * 
  */
-#define YMODEM_FLASH_SIZE				(256 * 1024)	// 256 kB
+#define YMODEM_FLASH_SIZE				(256 * 1024) // 256kB
 
 /**
  * @brief  Starting address of flash
@@ -45,10 +45,10 @@
  * @brief  Flash sector number
  * 
  */
-#define YMODEM_FLASH_SECTOR_NUM			6 
+#define YMODEM_FLASH_SECTOR_NUM			(6) 
 
 /**
  * @brief  Num of flash sectors 
  * 
  */
-#define YMODEM_FLASH_NUM_OF_SECTORS		1
+#define YMODEM_FLASH_NUM_OF_SECTORS		(1)
